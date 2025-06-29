c17409cf0184653bcb535a348277b0288772dee6
Manasvi Jain <manasvij234@gmail.com>
2025-06-29 15:14:21 +0530

    fixed relative path issue

--- auth_service/crud.py
+++ auth_service/crud.py
@@ -1,5 +1,5 @@
-from .models import User
-from .utils import hash_password, verify_password
+from models import User
+from utils import hash_password, verify_password
 from sqlalchemy.future import select
 
 async def create_user(db, email: str, password: str):
